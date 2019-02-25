ed575bc2773a6e86d8afe96aa84b89cd0177787d
Chu Dinh Cuong <nukniol@gmail.com>
2019-02-26 01:17:01 +0900

    Update main.cpp

--- Janken/Janken/main.cpp
+++ Janken/Janken/main.cpp
@@ -4,19 +4,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MISENTAKU		0
-#define RESULT_PATTERN	3
-#define HAND_PATTERN	3
-#define FIX_HAND		1
-#define NUM_OVER		HAND_PATTERN
-
 enum RESULT
 {
 	DRAW,
 	WIN,
-	LOOSE
+	LOOSE,
+	RESULT_PARTEN
 };
-int checkHand[RESULT_PATTERN][RESULT_PATTERN] = {
+enum HAND
+{
+	NO_CHOOSE,
+	ROCK = 0,
+	SCISSORS,
+	PAPER,
+	HAND_PATTERN
+};
+
+HAND getHand(int move) {
+	switch (move)
+	{
+	case 1:
+		return ROCK;
+		break;
+	case 2:
+		return SCISSORS;
+		break;
+	case 3:
+		return PAPER;
+		break;
+	}
+	return NO_CHOOSE;
+}
+int checkHand[HAND_PATTERN][HAND_PATTERN] = {
 	//Player|Computerグー(0) チョキ(1) パー(2)
 	/*グー(0)*/		{ DRAW,		WIN,	LOOSE },
 	/*チョキ(1)*/	{ LOOSE,	DRAW,	WIN },
@@ -26,7 +45,7 @@ int main() {
 	int input, player, computer;
 	const char* message[] = { "引き分け","勝ち","負け" };
 	const char* hand[] = { "グー","チョキ","パー" };
-	int senseki[RESULT_PATTERN] = { NULL };
+	int senseki[RESULT_PARTEN] = { NULL };
 
 	while (true)
 	{
@@ -34,14 +53,14 @@ int main() {
 		printf("0:未選択、1:グー、2:チョキ、3:パー :->");
 		scanf("%d", &input);
 
-		if (input <= MISENTAKU || input > NUM_OVER) {
+		if (input <= NO_CHOOSE || input > HAND_PATTERN) {
 			printf("エラー\n");
 			printf("再選択してください!\n");
 		}
 		else {
 			srand((unsigned int)time(NULL));
-			player = input - FIX_HAND;
-			computer = rand() % HAND_PATTERN;
+			player = getHand(input);
+			computer = getHand(rand() % HAND_PATTERN);
 			printf("プレヤーの手：%s\nコンピューターの手：%s\n", hand[player], hand[computer]);
 
 			int result = checkHand[player][computer];
